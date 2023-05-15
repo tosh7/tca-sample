2384e1ccf0189caa677541b6736d95165b2f02cf
tosh7 <zlia.6.lj.425@gmail.com>
2023-05-15 15:07:12 +0900

    Initial Commit

---
+++ tca-sample/tca_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_sampleApp.swift
+//  tca-sample
+//
+//  Created by Satoshi Komatsu on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct tca_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
