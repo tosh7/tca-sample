5db534b3eb24b9ebbcc8d8484704e7119268d32c
tosh7 <zlia.6.lj.425@gmail.com>
2023-05-25 23:09:24 +0900

    Create Feature directory

--- tca-sample/Feature/Feature.swift
+++ tca-sample/Feature/Feature.swift
@@ -1,11 +1,11 @@
 //
-//  ContentView.swift
+//  Feature.swift
 //  tca-sample
 //
-//  Created by Satoshi Komatsu on 2023/05/15.
+//  Created by Satoshi Komatsu on 2023/05/25.
 //
 
-import SwiftUI
+import Foundation
 import ComposableArchitecture
 
 struct Feature: ReducerProtocol {
@@ -58,39 +58,3 @@ struct Feature: ReducerProtocol {
         }
     }
 }
-
-struct FeatureView: View {
-    let store: StoreOf<Feature>
-
-    var body: some View {
-        WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack {
-                HStack {
-                    Button("-") { viewStore.send(.decrementButtonTapped) }
-                    Text("\(viewStore.count)")
-                    Button("+") { viewStore.send(.incrementButtonTapped) }
-                }
-
-                Button("Number fact") { viewStore.send(.numberFactButtonTapped) }
-            }
-            .alert(
-                item: viewStore.binding(
-                    get: { $0.numberFactAlert.map(FactAlert.init(title:)) },
-                    send: .factAlertDismissed
-                ),
-                content: { Alert(title: Text($0.title)) }
-            )
-        }
-    }
-}
-
-struct FactAlert: Identifiable {
-    var id: String { self.title }
-    var title: String
-}
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        FeatureView()
-//    }
-//}
---
+++ tca-sample/Feature/FeatureView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  tca-sample
+//
+//  Created by Satoshi Komatsu on 2023/05/15.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct FeatureView: View {
+    let store: StoreOf<Feature>
+
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                HStack {
+                    Button("-") { viewStore.send(.decrementButtonTapped) }
+                    Text("\(viewStore.count)")
+                    Button("+") { viewStore.send(.incrementButtonTapped) }
+                }
+
+                Button("Number fact") { viewStore.send(.numberFactButtonTapped) }
+            }
+            .alert(
+                item: viewStore.binding(
+                    get: { $0.numberFactAlert.map(FactAlert.init(title:)) },
+                    send: .factAlertDismissed
+                ),
+                content: { Alert(title: Text($0.title)) }
+            )
+        }
+    }
+}
+
+struct FactAlert: Identifiable {
+    var id: String { self.title }
+    var title: String
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureView(store: Store(initialState: Feature.State()) {
+            Feature()
+        })
+    }
+}
