ea60fcaed45d63a0da823c207efc48a01119f8e1
tosh7 <zlia.6.lj.425@gmail.com>
2023-06-03 03:31:17 +0900

    Add boilerplate

--- tca-sample/SignIn/SignIn.swift
+++ tca-sample/SignIn/SignIn.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct SignIn: ReducerProtocol {
+
+    struct State: Equatable {}
+
+    struct Action: Equatable {}
+
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        .none
+
+    }
+
+}
--- tca-sample/SignIn/SignInView.swift
+++ tca-sample/SignIn/SignInView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct SignIn: View {
+struct SignInView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("")
     }
 }
 
 struct SignIn_Previews: PreviewProvider {
     static var previews: some View {
-        SignIn()
+        SignInView()
     }
 }
